029f6bf98304906f28c0a6a24c9fd8dec5e4bb03
Austin Andrews <admin@templarian.com>
2022-04-20 23:37:29 -0500

    commit config

--- webpack.config.js
+++ webpack.config.js
@@ -1,103 +1,13 @@
-const path = require('path');
-const fs = require('fs');
-const CopyPlugin = require("copy-webpack-plugin");
-const ExtraWatchWebpackPlugin = require('extra-watch-webpack-plugin');
-const {
-  dashToCamel,
-  getComponents
-} = require('./scripts/utils');
+const config = require('@pictogrammers/element-webpack');
 
-const DIST_DIR = 'dist';
-
-let filterComponents = [];
-
-// Limit demo to render only listed components:
-if (process.argv.length > 3 && process.argv[3]) {
-  const aComp = process.argv[3];
-  const aComps = aComp.split(/,/g);
-  aComps.forEach((aC) => {
-    if (aC.match(/^\w+([A-Z]|-)/) === null) {
-      throw new Error(`${aC} must be formatted as namespace-component or namespaceComponent`);
-    }
-  });
-  filterComponents = aComps.map(x => dashToCamel(x));
-}
-
-// Lists
-const components = getComponents('src');
-const inputs = [];
-
-components.forEach(({ input, examples }) => {
-  inputs.push(input);
-  examples.forEach(({ exampleInput }) => {
-    inputs.push(exampleInput);
-  });
-});
-
-module.exports = (env, argv) => {
-  const entries = [];
-  const mode = argv.mode === 'production' ? 'production' : 'development';
-  function addEntries(input, name) {
-    entries.push({
-      mode: mode,
-      entry: input,
-      module: {
-        rules: [
-          {
-            test: /\.(css|html)$/i,
-            use: 'raw-loader',
-          },
-          {
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-          },
-        ],
-      },
-      resolve: {
-        extensions: [ '.ts', '.js' ],
-      },
-      output: {
-        filename: `${name}.js`,
-        path: path.resolve(__dirname, DIST_DIR),
-      },
-      performance: {
-        hints: false
-      },
-    });
+module.exports = config({
+  port: 3000,
+  dist: 'dist',
+  watch: [],
+  before: (components, mode) => {
+    console.log(`${components.length} Components`);
+  },
+  after: (components, mode) => {
+    // Nothing
   }
-
-  /* if (mode === 'production') {
-    components.forEach(({ input, name }) => {
-      addEntries(input, name);
-    });
-  } */
-  addEntries(inputs, 'main');
-  // Output Basic Runtime
-  console.log(`Stats: ${components.length - 1} Components`);
-  entries[entries.length - 1].plugins = [
-    {
-      apply: (compiler) => {
-        compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
-          // components[0].name
-          // mode
-        });
-      }
-    },
-    new ExtraWatchWebpackPlugin({
-      files: [
-        'src/**/*.md',
-        'src/index.html'
-      ],
-    }),
-  ];
-
-  entries[entries.length - 1].devServer = {
-    static: {
-      directory: path.join(__dirname, DIST_DIR)
-    },
-    compress: true,
-    port: 3000
-  };
-  return entries;
-};
+});
