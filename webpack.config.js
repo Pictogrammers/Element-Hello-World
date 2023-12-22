cd2b2df0800d535ef19b2e26357f61192600f65c
Austin Andrews <admin@templarian.com>
2023-12-22 11:23:30 -0600

    upgrade to v0.0.27 of element

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const config = require('@pictogrammers/element-webpack');
 
-const bold = (text) => '\033[1m' + text + '\033[0m';
+const bold = (text) => '\x1b[1m' + text + '\x1b[0m';
 const green = (text) => '\x1b[32m' + text + '\x1b[0m';
 
 module.exports = config({
